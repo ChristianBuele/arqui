c48f81465a99f7793c09496067e62e6592c97320
Jonnathan Valdez <jonnathan.valdez@ucuenca.edu.ec>
2020-05-23 15:07:24 -0500

    sobrinoooooooooooooooooooo

--- arqui/src/main.java
+++ arqui/src/main.java
@@ -11,5 +11,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("holaaa");
+        System.out.println("sobrinoooooooooooooooooooooooooooooo");
     }
 }
